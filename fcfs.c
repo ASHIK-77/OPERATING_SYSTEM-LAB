3daca25f9fca174b35af35bfb0db59646bdd0922
Ashik s <ashikpersonal777@gmail.com>
2026-03-25 00:46:03 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,79 +1,72 @@
 #include <stdio.h>
 
 struct process {
-    int pid, at, bt, pri, ct, tat, wt, done;
+    int pid, at, bt, ct, tat, wt;
 };
 
 int main() {
-    int n, i, time = 0, completed = 0;
+    int n, i, j;
     float avg_wt = 0, avg_tat = 0;
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    struct process p[n];
-    int gantt[n], gt[n + 1], k = 0;
+    struct process p[n], temp;
 
+    // Input
     for(i = 0; i < n; i++) {
-        printf("Enter AT, BT and Priority of P%d: ", i + 1);
-        scanf("%d%d%d", &p[i].at, &p[i].bt, &p[i].pri);
+        printf("Enter Arrival Time and Burst Time of P%d: ", i + 1);
+        scanf("%d%d", &p[i].at, &p[i].bt);
         p[i].pid = i + 1;
-        p[i].done = 0;
     }
 
-    gt[0] = 0;
-
-    while(completed < n) {
-        int index = -1;
-
-        for(i = 0; i < n; i++) {
-            if(p[i].at <= time && p[i].done == 0) {
-                if(index == -1 ||
-                   p[i].pri < p[index].pri ||
-                   (p[i].pri == p[index].pri && p[i].at < p[index].at)) {
-                    index = i;
-                }
+    // Sort based on Arrival Time
+    for(i = 0; i < n - 1; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(p[i].at > p[j].at) {
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
             }
         }
-
-        if(index == -1) {
-            time++;
-        } else {
-            gantt[k] = p[index].pid;
-            time += p[index].bt;
-            gt[k + 1] = time;
-            k++;
-
-            p[index].ct = time;
-            p[index].tat = p[index].ct - p[index].at;
-            p[index].wt = p[index].tat - p[index].bt;
-            p[index].done = 1;
-
-            avg_wt += p[index].wt;
-            avg_tat += p[index].tat;
-            completed++;
-        }
     }
 
-    printf("\nPID\tAT\tBT\tPRI\tCT\tTAT\tWT\n");
+    int time = 0;
+
+    // FCFS Calculation
     for(i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
-               p[i].pid, p[i].at, p[i].bt, p[i].pri,
+
+        if(time < p[i].at) {
+            time = p[i].at;  // CPU idle
+        }
+
+        p[i].ct = time + p[i].bt;
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
+
+        time = p[i].ct;
+
+        avg_wt += p[i].wt;
+        avg_tat += p[i].tat;
+    }
+
+    // Output Table
+    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
+    for(i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
+               p[i].pid, p[i].at, p[i].bt,
                p[i].ct, p[i].tat, p[i].wt);
     }
 
     printf("\nAverage Waiting Time = %.2f", avg_wt / n);
     printf("\nAverage Turnaround Time = %.2f\n", avg_tat / n);
 
+    // Gantt Chart
     printf("\nGantt Chart:\n");
-    for(i = 0; i < k; i++) {
-        printf("|  P%d  ", gantt[i]);
-    }
-    printf("|\n");
 
-    printf("%d", gt[0]);
-    for(i = 1; i <= k; i++) {
-        printf("      %d", gt[i]);
+    printf("%d", p[0].at);
+    for(i = 0; i < n; i++) {
+        printf(" | P%d | %d", p[i].pid, p[i].ct);
     }
     printf("\n");
 
